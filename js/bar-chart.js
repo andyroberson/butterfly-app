14384ab369638399ae1138c190cc46147d2da3e1
Andy Roberson <andyroberson2@gmail.com>
2016-04-29 15:20:32 -0400

    styling, making responsive

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,3 +1,5 @@
+
+
 var margin = {top: 40, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -27,7 +29,7 @@ var tip = d3.tip()
     return "<strong>hectares:</strong> <span style='color:orangered'>" + d.hectares + "</span>";
   })
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("bar-chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
--- js/migration-map.js
+++ js/migration-map.js
@@ -5,22 +5,6 @@ var i;
 var width = 860,
     height = 500;
 
-var mq = window.matchMedia( "(min-width: 860px)" );
-
-//responsiveness
-if (mq.matches) {
-  // window width is at least 860px
-  var width = 860,
-      height = 500;
-} else {
-// window width is less than 500px
-  var width = 460,
-      height = 200;
-
-  var projection = d3.geo.mercator()
-      .scale(200);
-}
-
 //reporting that the button was clicked, and so set i to 0 to set the animation (which can't start until i has been set)
 function report() {
      i = 0;
@@ -75,7 +59,7 @@ g.selectAll("path")
       //dateText shows what's happening to the Monarch during the migration depending on the Month
       var dateText = svg.append("text")
         .attr("id", "dataTitle")
-        .text("2013/11/"+track[0].month + ": " + track[0].action)
+        .text(track[0].month + ": " + track[0].action)
         .attr("x", 70)
         .attr("y", 20)
         .attr("font-family", "sans-serif")
@@ -111,7 +95,7 @@ g.selectAll("path")
 
       var pt = migrationRouteEl.getPointAtLength(0);
 
-      //butterfly icon 1
+      //butterfly "d" 1
       var icon = svg.append("path")
       .attr("d", "m 0,0 c 0,0 -1.785,9.652 -5.295,6.992 m 11.458,0 C 2.652,9.652 0.868,0 0.868,0 m -2.624,-2.933 -0.019,-28.673 c 0,0 2.163,-5.271 4.672,0.056 L 2.878,-2.903 m 0.057,-6.748 c 0,0 11.864,17.454 27.875,15.7 0,0 9.222,-0.924 5.712,-5.749 0,0 -4.601,-1.353 -6.794,-6.617 -2.193,-5.264 -5.997,-11.662 -9.576,-11.662 l -17.255,0 c 0,0 21.133,-0.17 21.572,-15.524 0,0 0,-4.287 -5.922,-3.41 -5.922,0.877 -16.166,-1.595 -15.65,5.423 L 2.878,-2.903 c 0,0 0.096,2.924 -2.097,2.924 l -0.44,0 c -2.193,0 -2.097,-2.954 -2.097,-2.954 l -0.019,-28.675 c 0.516,-7.018 -9.728,-4.577 -15.65,-5.454 -5.922,-0.877 -5.922,3.545 -5.922,3.545 0.439,15.353 21.572,15.538 21.572,15.538 l -17.255,0 c -3.579,0 -7.383,6.398 -9.576,11.662 -2.193,5.264 -6.795,6.737 -6.795,6.737 -3.509,4.825 5.713,5.689 5.713,5.689 16.011,1.754 27.874,-15.73 27.874,-15.73")
       .attr("transform",  "translate(" + pt.x + "," + pt.y + "), scale(.5), rotate(180)")
